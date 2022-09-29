550dd1ee45187dfbdacbd2f8b930751865faa4cf
shaikmoahmmedisaq <18tbeece046@kahedu.edu.in>
2022-09-29 21:15:47 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+def app_isaq(resquest):
+    return HttpResponse('<marquee><h1><i><b>shaik mohammedisaq</b></i></h1></marquee>')
